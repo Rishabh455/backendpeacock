4d1b65199fee014d5ce6d663d64e88a1e92a59a2
rishabh banana <rishabhchourasia858@gmail.com>
2023-02-21 20:32:24 +0530

    first commit

---
+++ Modals/UserData.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose;
+const UserDataSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    number: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    token: {
+        type: String,
+        default: ''
+    }
+})
+
+module.exports = mongoose.model("user", UserDataSchema)
